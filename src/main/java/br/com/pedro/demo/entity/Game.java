80f7184da3c8c1c70bd4398ce83c20a16adfab88
João Pedro Guimarães <jjppgs@gmail.com>
2026-02-05 21:00:29 -0300

    feat: Altera a entidade Game com seus atributos e relacionamentos.

--- src/main/java/br/com/pedro/demo/entity/Game.java
+++ src/main/java/br/com/pedro/demo/entity/Game.java
@@ -3,12 +3,16 @@ package br.com.pedro.demo.entity;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -41,15 +45,24 @@ public class Game {
     private String desenvolvedora;
     private String publicadora;
 
+    // @JoinTable: Cria a tabela intermediária no banco
+    // name: nome da tabela; joinColumns: ID do Game; inverseJoinColumns: ID do
+    // É criado uma tabela chamada game_generos com as colunas game_id e genero_id
+    // Como eu nao irei adicionar colunas extras na tabela intermediária, eu nao
+    // preciso criar uma classe para ela
     @ManyToMany
+    @JoinTable(name = "game_generos", joinColumns = @JoinColumn(name = "game_id"), inverseJoinColumns = @JoinColumn(name = "genero_id"))
     private List<Genero> generos;
 
     @ManyToMany
+    @JoinTable(name = "game_plataformas", joinColumns = @JoinColumn(name = "game_id"), inverseJoinColumns = @JoinColumn(name = "plataforma_id"))
     private List<Plataforma> plataformas;
 
     @ManyToMany
+    @JoinTable(name = "game_modos_de_jogo", joinColumns = @JoinColumn(name = "game_id"), inverseJoinColumns = @JoinColumn(name = "modo_id"))
     private List<ModoDeJogo> modosDeJogo;
 
-    @ManyToMany
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "game_id")
     private List<Screenshot> screenshots;
 }
